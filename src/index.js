fd45c1ff93f750544bf614eadd202f234e388a8c
lingtheroof <122776634+lingtheroof@users.noreply.github.com>
2023-01-18 10:14:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ class Root extends Component {
                 <HashRouter basename="/">
 	                <div>
 	                <Switch>
-	                    <Route exact path="/" component={HomeV11} />
+	                    <Route exact path="/" component={HomeV10} />
                       
 
                         <Route path="/about" component={About} />
